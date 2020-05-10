b354a5cf26876717c09dc4087f939c702b77f2ea
zeez <zeezrdq@gmail.com>
2020-05-10 23:37:51 +0200

    added a header that will contain the links for the Todos and Done Todos views

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app">
+    <Header />
     <router-view />
   </div>
 </template>
 
 <script>
+import Header from "./components/layout/Header";
 export default {
   name: "App",
-  components: {}
+  components: {
+    Header
+  }
 };
 </script>
 <style>
---
+++ src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>
+      This is the TodoList App For InstaBug Task
+    </p>
+  </div>
+</template>
