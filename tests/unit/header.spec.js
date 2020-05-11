7ad7f32a98e5656fec1e75135c6b0e3fd1cfa5b3
zeez <zeezrdq@gmail.com>
2020-05-11 23:24:01 +0200

    edited Header unit test file

--- tests/unit/header.spec.js
+++ tests/unit/header.spec.js
@@ -1,4 +1,5 @@
-import { mount } from "@vue/test-utils";
+import { shallowMount, mount, createLocalVue } from "@vue/test-utils";
+
 import Header from "@/components/layout/Header.vue";
 
 describe("Header Component unit tests: ", () => {
@@ -6,16 +7,5 @@ describe("Header Component unit tests: ", () => {
   test("is a vue instance", () => {
     const wrapper = mount(Header);
     expect(wrapper.isVueInstance()).toBeTruthy();
-  });
-
-  // check that he is in the correct route
-  test("different view is shown when header links is clicked", () => {
-    const wrapper = mount(Header);
-    wrapper.find("#todos").trigger("click");
-    expect(wrapper.vm.route).toBe("Todos");
-    wrapper.find("#home").trigger("click");
-    expect(wrapper.vm.route).toBe("Home");
-    wrapper.find("#done").trigger("click");
-    expect(wrapper.vm.route).toBe("Done");
   });
 });
